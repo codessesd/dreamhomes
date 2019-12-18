d630cb698cefd3bee4082079834eab1b2e2c9c28
shanern <rendyshane@gmail.com>
2019-12-18 21:10:43 +0200

    designing the header

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Dream Homes Stokvel</title>
+  <link rel="stylesheet" type="text/css" href="css/styles.css">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat|Raleway&display=swap" rel="stylesheet">
+</head>
+<body>
+  <div class="website">
+    <div class="info-bar"></div>
+
+    <div class="header-container">
+      <div class="logo">
+        <img src="/images/logo.png" alt="dream homes stokvel">
+      </div>
+      <div class="menu-container">
+        {{-- <div> --}}
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+            <li><a href="#">Register</a></li>
+          </ul>
+        {{-- </div> --}}
+        <span class="btn login"><a href="#">Login</a></span>
+      </div>
+
+    </div>
+  </div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function () {return view('index');});
+Route::get('about', function () {return view('about');});
+Route::get('register', function () {return view('register');});
+Route::get('contact', function () {return view('contact');});
